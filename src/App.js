b5c6339ae7b83770382d33fd3d1b9ebf346841a4
moisessdelmoro <moisesdelmoro@gec.inatel.br>
2021-05-28 13:20:12 -0300

    init cadastro

--- src/App.js
+++ src/App.js
@@ -1,31 +1,11 @@
-import React from 'react';
+import './App.css';
 
-function App (props) {
-
-  const modificarNome = event => {
-    console.log(event.target.value)
-  }  
-  const criaComboBox = () => {
-    const opcoes = ["Moises", "Delmoro", "Santos"]
-    const comboBoxOpcoes = opcoes.map(opcao=><option>{opcao}</option>)
-
-    return (
-      <select>
-        {comboBoxOpcoes}
-      </select>
-    )
-  }
-
-    const MeuComboBox = () => criaComboBox()
-    
-    return (
-      <>
-      <input type="text" value={props.nome} onChange={modificarNome} />
-      <h1>Hello {props.nome} </h1>
-      <MeuComboBox />
-      </>
-    
-    )
+function App() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css';
 import App from './App';
 
-ReactDOM.render(<App nome="Moises passado via props"/>, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
+
